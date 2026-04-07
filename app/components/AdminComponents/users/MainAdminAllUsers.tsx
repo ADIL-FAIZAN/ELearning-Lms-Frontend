6f4ed2d3bb7327f742f47c03c3151b79468ef87c
Adil Faizan <adilrana409@gmail.com>
2026-04-07 08:52:02 +0500

    changes in design

--- app/components/AdminComponents/users/MainAdminAllUsers.tsx
+++ app/components/AdminComponents/users/MainAdminAllUsers.tsx
@@ -12,6 +12,13 @@ const MainAdminAllUsers = (props: Props) => {
 
   return (
     
+
+    <>
+      
+      <div className="w-full max-md:pr-3 md:px-10">
+      <AdminDashboardHeader />  
+      </div>
+
     <div className="grid grid-cols-12">
          
    <div className="col-span-2">
@@ -19,8 +26,9 @@ const MainAdminAllUsers = (props: Props) => {
    </div>
    
    <div className="col-span-10 xl:px-10">
-    
-   <AdminDashboardHeader />       
+
+   <div className="flex justify-center font-bold text-xl md:hidden"> All Users</div>     
+
    <Allusers/>
    
     </div>
@@ -28,6 +36,7 @@ const MainAdminAllUsers = (props: Props) => {
    
     </div>
   
+    </>
   )
 }
 
