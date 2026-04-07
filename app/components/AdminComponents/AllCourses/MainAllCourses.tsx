f4f91a8b5278f2da79b081b9701693bfb7a20989
Adil Faizan <adilrana409@gmail.com>
2026-04-07 09:29:25 +0500

    changes in design

--- app/components/AdminComponents/AllCourses/MainAllCourses.tsx
+++ app/components/AdminComponents/AllCourses/MainAllCourses.tsx
@@ -13,22 +13,28 @@ const MainAllCourses = (props: Props) => {
 
     return (
       
+    <>
+    
+    <div className="w-full max-md:pr-3 md:px-10">
+    <AdminDashboardHeader />  
+    </div>
+
    <div className="grid grid-cols-12">
          
    <div className="col-span-2">
    <AdminSidebar />
    </div>
    
-   <div className="col-span-10  px-10">
-   <AdminDashboardHeader />          
+   <div className="col-span-10 xl:px-10">
+                 
+   <div className="flex justify-center font-bold text-xl md:hidden"> All Courses</div>              
    <AllCourses/>
    
     </div>
    
-   
     </div>
 
-    
+    </>
     )
 }
 
